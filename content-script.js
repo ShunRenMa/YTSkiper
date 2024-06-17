4d71cbffac929aa07e1f688e3399e3bb0178cb4b
Baird Ma <e12813726@gmail.com>
2024-06-17 12:16:49 +0800

    防ADB 視窗

--- content-script.js
+++ content-script.js
@@ -18,18 +18,26 @@ const ad_skip_span = [
     "ytp-preview-ad"
 ]
 
-// 重新整理網頁
-const ad_b_window_attr = ["允許放送 YouTube 廣告","重新整理網頁"]
+// ADB 第一頁
+// const ad_b_window_attr = ["允許放送 YouTube 廣告","重新整理網頁"]
+const ad_b_window_attr = ["允許放送 YouTube 廣告"]
 
-const videoDiv = "html5-video-container"
+const videoDiv = "html5-video-container";
+
+// 出現 ADB 時的背景遮罩
+const bg_block_cover_tag = "tp-yt-iron-overlay-backdrop";
+
+// ADB iframe
+const adb_iframe_div_class = ['fc-whitelist-dialog']
 
 const url = window.location.href
+
 if (url.indexOf("youtube") > -1) {
     setInterval(() => {
         for (const e of ad_skip_span) {
             let ad_skip_area = document.getElementsByClassName(e)
             if (ad_skip_area[0]) {
-                let divs = document.getElementsByClassName(videoDiv)
+                const divs = document.getElementsByClassName(videoDiv)
                 for (const div of divs) {
                     let video = div?.getElementsByTagName("video")[0];
                     if (video) {
@@ -51,9 +59,34 @@ if (url.indexOf("youtube") > -1) {
         for(const e of ad_b_window_attr ){
             const elements = document.querySelectorAll(`[aria-label='${e}']`);
             if (elements?.[0]) {
-                console.log("[tracking] ad skip for adb")
+                console.log("[tracking] ad skip for adb ",elements[0])
                 elements[0].click()
             }
         }
+
+        
+
+        for (const e of adb_iframe_div_class) {
+            const elements = document.getElementsByClassName(e);
+            if (elements?.[0]) {
+                console.log("[tracking] ad skip for adb iframe ",elements[0])
+               
+                elements[0].remove()
+                const video = document.getElementsByClassName(videoDiv)[0].getElementsByTagName("video")[0];
+                
+                setInterval((i) => {
+                    if(video?.paused){
+                        video.play();
+                    }else{
+                        clearInterval(i);
+                    }
+                },500);
+               
+            }
+        }
+        
+        document.getElementsByTagName(bg_block_cover_tag)[0]?.remove();
+      
+
     }, 250)
 }
