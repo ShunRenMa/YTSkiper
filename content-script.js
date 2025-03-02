ea681980d87f1b5eccf2d164681820654781715f
Baird Ma <e12813726@gmail.com>
2025-03-02 14:56:47 +0800

    update

--- content-script.js
+++ content-script.js
@@ -25,61 +25,59 @@ const ad_b_window_attr = ["允許放送 YouTube 廣告",'贊助商廣告']
 const videoDiv = "html5-video-container";
 
 // 出現 ADB 時的背景遮罩
-const bg_block_cover_tag = "tp-yt-iron-overlay-backdrop";
+const bg_block_cover_tag = "tp-yt-iron-overlay-backdrop"; 
 
 // ADB iframe
 const adb_iframe_div_class = ['fc-whitelist-dialog']
 // video-ads
 
 const url = window.location.href
+let timer
 let hasAD = false;
 let videoCurrentTime = 0;
-if (url.indexOf("youtube") > -1) {
-    setInterval(() => {
+if (url.indexOf("youtube") > -1 ) {
+    document.getElementsByTagName("ytd-watch-flexy")[0] ? mainFun(true) :
+    isElementVisible(document.getElementsByTagName("ytd-watch-flexy")[0], mainFun)
+}
 
-        // for (const e of ad_skip) {
-        //     const elements = document.getElementsByClassName(e);
-        //     if (elements?.[0]) {
-        //         // console.log("[tracking] ad skip click")
-        //         // elements[0].click()
-        //         // findVideoAndSetTimeToSkip()
-        //         // hasAD = true;
-        //     }
-        // }
-
-        for(const e of ad_b_window_attr ){
-            const elements = document.querySelectorAll(`[aria-label='${e}']`);
-            if (elements?.[0]) {
-                console.log("[tracking] ad skip for adb ",elements[0])
-                // elements[0].click()
-                findVideoAndSetTimeToSkip()
-                hasAD = true;
+function mainFun(v){
+    if(!timer){
+        console.log("observer," ,v)
+         timer = setInterval(() => {
+            // console.log("url = ",url)
+            for(const e of ad_b_window_attr ){
+                const elements = document.querySelectorAll(`[aria-label='${e}']`);
+                const adCTA = document.querySelector(".ytp-ad-button-vm__text");
+                if (adCTA) {
+                    // console.log("[tracking] ad skip for adb ",elements[0])
+                    console.log("[tracking] ad skip for adb ",adCTA)
+                    // elements[0].click()
+                    findVideoAndSetTimeToSkip()
+                    hasAD = true;
+                }
             }
-        }
 
-        
-
-        for (const e of adb_iframe_div_class) {
-            const elements = document.getElementsByClassName(e);
-            const video = document.querySelector(`.${videoDiv} video`);
-          
-            if(video && hasAD){
-                videoCurrentTime = video.currentTime === 0 ? videoCurrentTime : video.currentTime;
-                console.log("video c = ",videoCurrentTime);
+            for (const e of adb_iframe_div_class) {
+                const elements = document.getElementsByClassName(e);
+                const video = document.querySelector(`.${videoDiv} video`);
+            
+                if(video && hasAD){
+                    videoCurrentTime = video.currentTime === 0 ? videoCurrentTime : video.currentTime;
+                    console.log("video c = ",videoCurrentTime);
+                }
+                if (elements?.[0]) {
+                    console.log("[tracking] ad skip for adb iframe ",elements[0])
+                    const targetTimeParam = `&t=${Math.ceil(videoCurrentTime)}s`;
+                    const isLive = !!document.querySelector(".ytp-live");
+                    window.location.href = `${window.location.href.replace(/&t=\d+s/, '')}${isLive ? '' : targetTimeParam}`;
+                    break;
+                }
             }
-            if (elements?.[0]) {
-                console.log("[tracking] ad skip for adb iframe ",elements[0])
-                const targetTimeParam = `&t=${Math.ceil(videoCurrentTime)}s`;
-                const isLive = !!document.querySelector(".ytp-live");
-                window.location.href = `${window.location.href.replace(/&t=\d+s/, '')}${isLive ? '' : targetTimeParam}`;
-                break;
-            }
-        }
 
-        hasAD = false;
-        document.getElementsByTagName(bg_block_cover_tag)[0]?.remove();
-
-    }, 250)
+            hasAD = false;
+            document.getElementsByTagName(bg_block_cover_tag)[0]?.remove();
+        }, 250)
+    }
 }
 
 
@@ -103,4 +101,12 @@ function setBodyOverflow() {
     });
 }
 
-// document.getElementsByClassName("ytp-ad-skip-button-modern ytp-button")[0]?.click()
+function isElementVisible(element, callback) {
+    console.log("element = ",element)
+    if (!element) return false;
+    const observer = new IntersectionObserver((entries) => {
+        callback(entries[0].isIntersecting);
+    });
+
+    observer.observe(element);
+}
